7261e9d977f51a26e11f7d731fd5064d61c690df
quang1409thanh <97092690+quang1409thanh@users.noreply.github.com>
2022-04-17 03:47:40 +0700

    commit load image bmp

--- main.cpp
+++ main.cpp
@@ -9,48 +9,94 @@ and may not be redistributed without written permission.*/
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int main( int argc, char* args[] )
-{
-	//The window we'll be rendering to
-	SDL_Window* window = NULL;
-	
-	//The surface contained by the window
-	SDL_Surface* screenSurface = NULL;
+// start up SDL and create window
+bool init();
 
-	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+//load media
+bool loadMedia();
+
+// Free media and shuts downs SDL
+void close();
+
+// the windowns we'll be rendering to-của sổ mà chúng tôi sẽ hiện thị cho
+SDL_Window* gWindow=NULL;
+
+// the surface contained by the windows-bề mặt được chứa bởi các cửa sổ
+SDL_Surface* gScreenSurface=NULL;
+
+// the image we will load and show on the screen-hình ảnh mà chúng tôi sẽ tải và hiển thị trên màn hình
+SDL_Surface* gHelloWorld=NULL;
+
+bool init(){
+    //Initialization flag-biến đánh dấu khởi tạo
+    bool success = true;
+    //Initialization SDL-khởi tạo SDL
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
 	{
 		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+        success=false;
 	}
 	else
 	{
-		//Create window
-		window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( window == NULL )
+		//Create window-tạo cửa sổ
+		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		if( gWindow == NULL )
 		{
 			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+            success=false;
 		}
 		else
 		{
-			//Get window surface
-			screenSurface = SDL_GetWindowSurface( window );
+			//Get window surface-lấy bề mặt của cửa sổ
+			gScreenSurface = SDL_GetWindowSurface( gWindow );
+        }
+    }
+    return success;
+}
 
-			//Fill the surface white
-			SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-			
-			//Update the surface
-			SDL_UpdateWindowSurface( window );
+bool loadMedia(){
+    // Loading success flag -biến đánh dấu tải thành công
+    bool success = true;
+    // load splash image-tải hình ảnh splash
+    gHelloWorld = SDL_LoadBMP("dot.bmp");
+    if( gHelloWorld == NULL){
+        printf("Unable to load image %s! SDL Error: %s\n", "dot.bmp", SDL_GetError());
+        success = false ;
+    }
+    return success ;
+}
 
-			//Wait two seconds
-			SDL_Delay( 2000 );
-		}
-	}
+void close(){
+    //Deallocate surface - hủy bề mặt
+    SDL_FreeSurface( gHelloWorld);
+    gHelloWorld = NULL;
+    //Destroy window - hủy cửa sổ
+    SDL_DestroyWindow( gWindow);
+    gWindow = NULL;
+    // Quit SDL subsystems - thoát các hệ thống phục vụ SDL
+    SDL_Quit();
+}
 
-	//Destroy window
-	SDL_DestroyWindow( window );
-
-	//Quit SDL subsystems
-	SDL_Quit();
-
-	return 0;
+int main( int argc, char* args[] )
+{
+	//Start up SDL and create window
+    if(!init()){
+        printf("Failed to initialize!\n");
+    }
+    else {
+        if(!loadMedia()){
+            printf("Failed to load media!\n");
+        }
+        else {
+            //Apply the image -áp dụng hình ảnh
+            SDL_BlitSurface( gHelloWorld, NULL, gScreenSurface, NULL);
+            //Update the surface - cập nhật bề mặt
+            SDL_UpdateWindowSurface( gWindow);
+            //Wait two seconds - chờ 2 giây
+            SDL_Delay( 2000 );
+        }
+    }
+    // Free resources and close SDL-hủy các tài nguyên và đóng SDL
+    close();
+    return 0;
 }
